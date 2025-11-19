9b19004dcd0683c881a50c2491fded0385fc5123
Laxmi-gupta <lgupta6972@gmail.com>
2025-11-19 19:33:17 +0530

    prisma client setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
+console.log(prisma);
 
 app.get('/',(req,res) => {
   res.send("root route")
